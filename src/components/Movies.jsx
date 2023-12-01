47d7487ace4c13719af1b50bb756663ae8db8374
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-11-30 22:34:04 -0300

    add custom hooks

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 
-export function ListOfMovies ({ movies }) {
+function ListOfMovies ({ movies }) {
   return (
-    <ul>
-      {movies.map(movie => (
-        <li key={movie.id}>
-          <h3>{movie.title}</h3>
-          <p>{movie.year}</p>
-          <img src={movie.poster} alt={movie.Title} />
-        </li>
-      ))}
+    <ul className='movies'>
+      {
+        movies.map(movie => (
+          <li className='movie' key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+            <img src={movie.image} alt={movie.title} />
+          </li>
+        ))
+      }
     </ul>
   )
 }
 
-export function NoMovies () {
-  return <p>Sorry, no movies found</p>
+function NoMoviesResults () {
+  return (
+    <p>No se encontraron películas para esta búsqueda</p>
+  )
 }
 
 export function Movies ({ movies }) {
   const hasMovies = movies?.length > 0
 
-  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMovies />
+  return (
+    hasMovies
+      ? <ListOfMovies movies={movies} />
+      : <NoMoviesResults />
+  )
 }
